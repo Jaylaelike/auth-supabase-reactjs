f954cafcf96867f08d502738fc163cc9a9454c0b
Jaylaelike <smarkwisai@gmail.com>
2023-06-30 00:29:04 +0700

    first commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zcrovytsopigxzsmhgsu.supabase.co";
-const supabaseAnonkey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpjcm92eXRzb3BpZ3h6c21oZ3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ0ODMwNTUsImV4cCI6MTk3MDA1OTA1NX0._OLnD_JLMXjumCWsrd39nt01-02__xcvfQkpqFdQSao";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonkey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
